323e9b184481f4b7972bf3bde2fbf33ca63b8140
Michael(Qingda) Mai <michael.mai@uwaterloo.ca>
2025-07-05 13:27:46 -0400

    Week 2 - CS 246 Referencing & memory allocation

--- CS 246/main.cpp
+++ CS 246/main.cpp
@@ -4,6 +4,26 @@
 using namespace std;
 
 
+const double PI = 3.14159;
+
+struct Point {
+    int x;
+    int y;
+};
+
+void increment(int x ) {
+    x++;
+}
+
+void increment_passed_by_x (int &x) {
+    x++;
+}
+
+void increment_passed_by_pointer (int *x) {
+    (*x)++;
+}
+
+
 /**
  * @file main.cpp
  * @brief Main function to demonstrate the PizzaShop class and other functionalities.
@@ -24,6 +44,40 @@ using namespace std;
 
 
 int main() {
+
+
+    int a = 10;
+    int &ref = a; // ref is a reference to a
+    ref = 20;     // Modifies a directly
+    std::cout << a; // Output: 20
+
+
+    int *ptr = new int;  // Dynamically allocate memory for an integer
+    *ptr = 42;           // Assign a value
+    std::cout << *ptr;   // Output: 42
+    delete ptr;          // Free the allocated memory
+
+
+
+    int num = 5; // Variable declaration and initialization
+    increment(num);
+    std::cout << num << std::endl; // Output the value of num
+
+    increment_passed_by_x(num); // value will be changed
+    std::cout << num << std::endl; // Output the value of num
+
+    num = 10;
+    increment_passed_by_pointer(&num);
+    std::cout << num; // Output: 6 (original value changed)
+
+    std::cout << "Value of PI: " << PI << std::endl; // Constants
+    // Constants are immutable, so we cannot change PI
+
+    // Structs
+    // A struct is a user-defined data type that allows grouping of variables
+    Point p = {10, 20};
+    std::cout << "Point: (" << p.x << ", " << p.y << ")" << std::endl;
+
     greet(); // method 1
 
     // PizzaShop shop;
